f05d55bf36e0cc15770175f35f9ea76054396e65
João Victor Alves da Silva <jvralvees@gmail.com>
2025-05-15 20:14:55 -0300

    update

---
+++ src/Node.java
@@ -0,0 +1,12 @@
+public class Node {
+    String value;
+    Node left;
+    Node right;
+
+    public Node(String valor) {
+        this.value = valor;
+        this.left = null;
+        this.right = null;
+    }
+}
+
